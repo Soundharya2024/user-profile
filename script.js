20f1a743bf6d8fa31132723fbbba7ec46ef3f7bc
Soundharya S <happie.sou@gmail.com>
2024-09-27 20:23:59 +0530

    card-4, hovers, card gaps, responsive

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ $(".dropdown-menu li a").click(function(){
   const selText = $(this).text();
   $(this).parents('.dropdown').find('.dropdown-bs-toggle').html(selText + ' <i class="d-inline-block bi bi-chevron-down ms-1"></i>');
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
